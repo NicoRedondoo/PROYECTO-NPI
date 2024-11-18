355a3d300408fa5c702fa56ae7c3baed87e3b2b8
teresaCL <teresacordobalillo@gmail.com>
2024-11-18 13:47:12 +0100

    Idiomas

--- app/src/main/java/com/example/proyectonpi/MainActivity.kt
+++ app/src/main/java/com/example/proyectonpi/MainActivity.kt
@@ -4,12 +4,26 @@ import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.constraintlayout.widget.ConstraintLayout
+import android.widget.Button
+import java.util.Locale
+
+
 
 
 // Asegúrate de que el paquete coincida con el de tu proyecto
 
 
 class MainActivity : AppCompatActivity() {
+    private fun setLanguage(languageCode: String) {
+        val locale = Locale(languageCode)
+        Locale.setDefault(locale)
+        val config = resources.configuration
+        config.setLocale(locale)
+
+        resources.updateConfiguration(config, resources.displayMetrics)
+        // Aquí podrías recargar la actividad o cambiar el contenido de la UI si es necesario
+        recreate()
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -17,15 +31,42 @@ class MainActivity : AppCompatActivity() {
         // Referencia al ConstraintLayout
         val mainLayout = findViewById<ConstraintLayout>(R.id.main_layout)
 
+        val btnSpanish = findViewById<Button>(R.id.btn_spanish)
+        val btnEnglish = findViewById<Button>(R.id.btn_english)
+        val btnFrench = findViewById<Button>(R.id.btn_french)
+        val btnGerman = findViewById<Button>(R.id.btn_german)
+        val btnItalian = findViewById<Button>(R.id.btn_italian)
+        val btnPortuguese = findViewById<Button>(R.id.btn_portuguese)
+
+        btnSpanish.setOnClickListener {
+            setLanguage("es")
+        }
+
+        btnEnglish.setOnClickListener {
+            setLanguage("en")
+        }
+
+        btnFrench.setOnClickListener {
+            setLanguage("fr")
+        }
+
+        btnGerman.setOnClickListener {
+            setLanguage("de")
+        }
+
+        btnItalian.setOnClickListener {
+            setLanguage("it")
+        }
+
+        btnPortuguese.setOnClickListener {
+            setLanguage("pt")
+        }
+
         // Configurar el listener de clic para detectar cuando se toca la pantalla
         mainLayout.setOnClickListener {
             // Mostrar un mensaje Toast cuando se toque la pantalla
-            Toast.makeText(this@MainActivity, "Pantalla tocada. Comenzando...", Toast.LENGTH_SHORT)
-                .show()
-
-            // Opcional: puedes agregar el Intent para pasar a la siguiente actividad
-            // Intent intent = new Intent(MainActivity.this, NextActivity.class);
-            // startActivity(intent);
+            val message = getString(R.string.screen_touch_message2)  // Usar la cadena traducida
+            Toast.makeText(this@MainActivity, message, Toast.LENGTH_SHORT).show()
         }
     }
 }
