d146199538a0543a42cb25a1f9f94207db9c8b14
= <=>
2024-12-21 17:02:53 +0100

    Añadido ayuda y arreglado comedor

--- app/src/main/java/com/example/proyectonpi/HelpActivity.kt
+++ app/src/main/java/com/example/proyectonpi/HelpActivity.kt
@@ -1,11 +1,23 @@
 package com.example.proyectonpi
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class HelpActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_help)
+        setupListeners()
+    }
+
+    private fun setupListeners() {
+        val volverButton = findViewById<Button>(R.id.btnBackToHome)
+        volverButton.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
